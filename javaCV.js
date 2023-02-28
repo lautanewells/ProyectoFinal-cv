69a19c1ce258c1c52b6fcaf054bfaaab8255e9cd
lautaro <lautaroojedamarquez@hotmail.com>
2023-02-27 19:33:04 -1100

    se agrega el proyecto

---
+++ javaCV.js
@@ -0,0 +1,65 @@
+document.getElementById("boton1").onclick = function (){
+    document.getElementById("boton1").innerHTML = "60 partidos / 20 goles / 0 asistencias";
+    document.getElementById("boton1").style.backgroundColor = "black";
+    document.getElementById("boton1").style.color = "white";
+}
+
+document.getElementById("boton2").onclick = function (){
+    document.getElementById("boton2").innerHTML = " partidos 111 / 26 goles / 1 asistencias";
+    document.getElementById("boton2").style.backgroundColor = "black";
+    document.getElementById("boton2").style.color = "white";
+}
+
+document.getElementById("boton3").onclick = function (){
+    document.getElementById("boton3").innerHTML = "154 partidos / 44 goles / 18 asistencias";
+    document.getElementById("boton3").style.backgroundColor = "black";
+    document.getElementById("boton3").style.color = "white";
+}
+
+document.getElementById("boton4").onclick = function (){
+    document.getElementById("boton4").innerHTML = "73 partidos / 17 goles / 7 asistencias";
+    document.getElementById("boton4").style.backgroundColor = "black";
+    document.getElementById("boton4").style.color = "white";
+}
+
+document.getElementById("boton5").onclick = function (){
+    document.getElementById("boton5").innerHTML = "156 partidos / 56 goles / 15 asistencias";
+    document.getElementById("boton5").style.backgroundColor = "black";
+    document.getElementById("boton5").style.color = "white";
+}
+
+document.getElementById("boton6").onclick = function (){
+    document.getElementById("boton6").innerHTML = "49 partidos / 21 goles / 5 asistencias";
+    document.getElementById("boton6").style.backgroundColor = "black";
+    document.getElementById("boton6").style.color = "white";
+}
+
+document.getElementById("boton7").onclick = function (){
+    document.getElementById("boton7").innerHTML = "69 partidos / 18 goles / 6 asistencias";
+    document.getElementById("boton7").style.backgroundColor = "black";
+    document.getElementById("boton7").style.color = "white";
+}
+
+document.getElementById("boton8").onclick = function (){
+    document.getElementById("boton8").innerHTML = "7 partidos / 4 goles / 0 asistencias";
+    document.getElementById("boton8").style.backgroundColor = "black";
+    document.getElementById("boton8").style.color = "white";
+}
+
+document.getElementById("boton9").onclick = function (){
+    document.getElementById("boton9").innerHTML = "5 partidos / 3 goles / 1 asistencias";
+    document.getElementById("boton9").style.backgroundColor = "black";
+    document.getElementById("boton9").style.color = "white";
+}
+
+document.getElementById("boton10").onclick = function (){
+    document.getElementById("boton10").innerHTML = "5 partidos / 0 goles / 0 asistencias";
+    document.getElementById("boton10").style.backgroundColor = "black";
+    document.getElementById("boton10").style.color = "white";
+}
+
+document.getElementById("boton11").onclick = function (){
+    document.getElementById("boton11").innerHTML = "2 partidos / 0 goles / 0 asistencias";
+    document.getElementById("boton11").style.backgroundColor = "black";
+    document.getElementById("boton11").style.color = "white";
+}
